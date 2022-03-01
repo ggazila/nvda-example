749c43066028597255b3d7a3cc534407078f4b98
Sergey Yevtushok <ggazila@gmail.com>
2022-03-01 18:10:25 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [value1, setValue1] = useState("default");
+  const [value2, setValue2] = useState("default");
+
+  const handleChatSearch = (e: any) => {
+    let randomString = `${(Math.random() + 1).toString(36).substring(7)}`;
+    let randomNumber = `${Math.floor(Math.random() * 10)}`;
+    let value = `Found ${randomNumber} results for ${randomString}`;
+    setValue1(value);
+  };
+
+  const handleChatSearch2 = (e: any) => {
+    let randomString = `${(Math.random() + 1).toString(36).substring(7)}`;
+    let randomNumber = `${Math.floor(Math.random() * 10)}`;
+    let value2 = `Found ${randomNumber} results for ${randomString}`;
+    setValue2(value2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleChatSearch}>
+        Click to search when aria-atomic=true
+      </button>
+      <div role="status" aria-atomic="true">
+        {value1}
+      </div>
+
+      <button onClick={handleChatSearch2}>
+        Click to search when aria-atomic=false
+      </button>
+      <div role="status">{value2}</div>
+    </>
   );
 }
 
